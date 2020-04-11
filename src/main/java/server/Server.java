e22a25c41c80518d7919fa4a7bf52badff58c9a4
Dmitry Savkin <dmitry.savkin.info@gmail.com>
2020-04-11 20:36:55 +0200

    implementing server and client

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,4 +1,103 @@
 package server;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+
+    private int port;
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    public void createServer() throws IOException, ClassNotFoundException {
+       ServerSocket server = new ServerSocket(port);
+
+        System.out.println("Shutting down Socket server!!");
+        while (true){
+            Thread t = new Thread(new ThreadServer(server));
+            t.start();
+        }
+ //       server.close();
+}
+
+
+    public static void main(String[] args) {
+        try {
+            new Server(9876).createServer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class ThreadServer implements  Runnable{
+
+    ServerSocket server;
+
+    public ThreadServer(ServerSocket server) {
+        this.server = server;
+    }
+
+    @Override
+    public void run() {
+
+        while(true){
+            System.out.println("Waiting for the client request");
+            //creating socket and waiting for client connection
+            Socket socket = null;
+            try {
+                socket = server.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //read from socket to ObjectInputStream object
+            ObjectInputStream ois = null;
+            try {
+                ois = new ObjectInputStream(socket.getInputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //convert ObjectInputStream object to String
+            String message = null;
+            try {
+                message = (String) ois.readObject();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Message Received: " + message);
+            //create ObjectOutputStream object
+            ObjectOutputStream oos = null;
+            try {
+                oos = new ObjectOutputStream(socket.getOutputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //write object to Socket
+            try {
+                oos.writeObject("Hi Client "+message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //close resources
+            try {
+                ois.close();
+                oos.close();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //terminate the server if client sends exit request
+            if(message.equalsIgnoreCase("exit")) break;
+        }
+    }
 }
--- src/main/java/server/ServerThread.java
+++ src/main/java/server/ServerThread.java
@@ -1,4 +1,28 @@
 package server;
 
-public class ServerThread {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerThread implements Runnable {
+
+    static ServerSocket ss = null;
+    static int port;
+    Thread t;
+    private static Socket socket = null;
+
+    public ServerThread(Socket socket) {
+        this.socket = socket;
+    }
+    @Override
+    public void run() {
+        DataInputStream dataIn = null;
+        DataOutputStream dataOut = null;
+        int i = 0;
+        while (i < 5) {
+            System.out.println("TTTTT");
+        }
+    }
 }
